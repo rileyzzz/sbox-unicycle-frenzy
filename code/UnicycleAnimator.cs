f02a9a5787959d7a78c789fe06a83ce471d13531
Crayz <jacob.ee92@gmail.com>
2021-11-22 00:27:55 -0600

    Rotate when in noclip

--- code/UnicycleAnimator.cs
+++ code/UnicycleAnimator.cs
@@ -4,6 +4,9 @@ internal class UnicycleAnimator : StandardPlayerAnimator
 {
 	public override void DoRotation( Rotation idealRotation )
 	{
-		//Rotation = idealRotation;
+		if ( Pawn is not Player pl ) return;
+		if ( pl.GetActiveController() is UnicycleController ) return;
+
+		Rotation = idealRotation;
 	}
 }
