ac1e401c7e775eb3d9dafa974d8cf7dcb0d261e5
Crayz <jacob.ee92@gmail.com>
2021-11-24 17:47:33 -0600

    Leaderboard model, entity, world panel

---
+++ code/Hammer/LeaderboardPost.cs
@@ -0,0 +1,23 @@
+﻿using Hammer;
+using Sandbox;
+
+[EditorModel("models/leaderboard_post.vmdl")]
+[Library( "uf_leaderboard", Description = "Shows a leaderboard in-game" )]
+[EntityTool( "Leaderboard Post", "Unicycle Frenzy", "Shows a leaderboard in-game" )]
+internal class LeaderboardPost : ModelEntity
+{
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		SetModel( "models/leaderboard_post.vmdl" );
+	}
+
+	public override void ClientSpawn()
+	{
+		base.ClientSpawn();
+
+		new LeaderboardSign( this );
+	}
+}
+
---
+++ code/UI/LeaderboardSign.cs
@@ -0,0 +1,41 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+[UseTemplate]
+internal class LeaderboardSign : WorldPanel
+{
+
+	private LeaderboardPost post;
+
+	public LeaderboardSign( LeaderboardPost post )
+	{
+		this.post = post;
+
+		Reposition();
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( !post.IsValid() )
+		{
+			Delete();
+			return;
+		}
+
+		Reposition();
+	}
+
+	private void Reposition()
+	{
+		var width = 2300;
+		var height = 1450;
+		MaxInteractionDistance = 5000f;
+		PanelBounds = new Rect( -width * .5f, -height * .5f, width, height );
+		Position = post.Position + post.Rotation.Forward * 4 + Vector3.Up * 82;
+		Rotation = post.Rotation;
+	}
+
+}
+
