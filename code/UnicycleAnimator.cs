b4463ba52e6ca73c2f2272898f1370a24c768367
Crayz <jacob.ee92@gmail.com>
2021-11-30 00:43:29 -0600

    Wheel spin for new modular parts

--- code/UnicycleAnimator.cs
+++ code/UnicycleAnimator.cs
@@ -1,40 +1,7 @@
 ﻿using Sandbox;
-using System;
 
 internal class UnicycleAnimator : StandardPlayerAnimator
 {
-
-	private float prevPedalPosition;
-
-	public override void FrameSimulate()
-	{
-		base.FrameSimulate();
-
-		if ( Pawn is not UnicyclePlayer pl || pl.Health <= 0 ) return;
-
-		// wheel
-		var tx = pl.GetBoneTransform( "Wheel" );
-		var radius = tx.Position.z - Position.z;
-		var distance = Velocity.Length;
-		var angle = distance * (180f / (float)Math.PI) / radius;
-		var dir = Math.Sign( Vector3.Dot( Velocity.Normal, Rotation.Forward ) );
-		var txRot = tx.WithRotation( tx.Rotation.RotateAroundAxis( Vector3.Left, angle * dir * Time.Delta ) );
-		pl.SetBone( "Wheel", txRot );
-
-		// pedals
-		// todo: lerp towards target rotation
-		var tx2 = pl.GetBoneTransform( "Pedals" );
-		var pedalDelta = pl.PedalPosition - prevPedalPosition;
-		if ( pedalDelta.AlmostEqual( 0f ) ) return;
-		var pedalAngle = MathF.Abs( pedalDelta ) * 90f;
-		var pedalRot = tx2.Rotation.RotateAroundAxis( Vector3.Left, pedalAngle );
-
-		var tx2Rot = tx2.WithRotation( pedalRot );
-		pl.SetBone( "Pedals", tx2Rot );
-
-		prevPedalPosition = pl.PedalPosition;
-	}
-
 	public override void DoRotation( Rotation idealRotation )
 	{
 		if ( Pawn is not UnicyclePlayer pl || pl.Health <= 0 ) return;
@@ -43,5 +10,4 @@ internal class UnicycleAnimator : StandardPlayerAnimator
 		// rotate when dev camera
 		base.DoRotation( idealRotation );
 	}
-
 }
--- code/UnicycleEntity.cs
+++ code/UnicycleEntity.cs
@@ -1,6 +1,5 @@
 ﻿using Sandbox;
 using System;
-using System.Numerics;
 
 internal partial class UnicycleEntity : Entity
 {
@@ -15,6 +14,8 @@ internal partial class UnicycleEntity : Entity
 	public ModelEntity SeatModel { get; set; }
 	[Net]
 	public ModelEntity WheelModel { get; set; }
+	[Net]
+	public Entity WheelPivot { get; set; }
 
 	public Vector3 GetAssLocalPosition()
 	{
@@ -41,6 +42,7 @@ internal partial class UnicycleEntity : Entity
 		FrameModel?.Delete();
 		SeatModel?.Delete();
 		WheelModel?.Delete();
+		WheelPivot?.Delete();
 
 		FrameModel = new ModelEntity( FramePath );
 		FrameModel.SetParent( this, null, Transform.Zero.WithScale( 1 ) );
@@ -48,8 +50,11 @@ internal partial class UnicycleEntity : Entity
 		SeatModel = new ModelEntity( SeatPath );
 		SeatModel.SetParent( FrameModel, "Seat", Transform.Zero.WithScale( 1 ) );
 
+		WheelPivot = new Entity();
+		WheelPivot.SetParent( FrameModel, "Wheel", Transform.Zero.WithScale( 1 ) );
+
 		WheelModel = new ModelEntity( WheelPath );
-		WheelModel.SetParent( FrameModel, "Wheel", Transform.Zero.WithScale( 1 ) );
+		WheelModel.SetParent( WheelPivot, null, Transform.Zero.WithScale( 1 ) );
 		var hub = WheelModel.GetAttachment( "Hub", false );
 
 		if ( hub.HasValue )
@@ -57,6 +62,27 @@ internal partial class UnicycleEntity : Entity
 			WheelModel.LocalPosition -= hub.Value.Position;
 			FrameModel.LocalPosition = Vector3.Up * hub.Value.Position.z - 2;
 		}
+	}
+
+	[Event.Tick.Server]
+	private void SpinWheel()
+	{
+		if ( Parent is not UnicyclePlayer pl ) return;
+		if ( !WheelModel.IsValid() ) return;
+
+		var radius = 12f;
+		var hub = WheelModel.GetAttachment( "Hub", false );
+
+		if ( hub.HasValue )
+		{
+			radius = hub.Value.Position.z;
+		}
+
+		var distance = pl.Velocity.Length;
+		var angle = distance * (180f / (float)Math.PI) / radius; // todo: why is this not right?
+		var dir = Math.Sign( Vector3.Dot( pl.Velocity.Normal, pl.Rotation.Forward ) );
+		angle *= .5f; 
+		WheelPivot.Rotation = WheelPivot.Rotation.RotateAroundAxis( Vector3.Left, angle * dir * Time.Delta );
 	}
 
 }
