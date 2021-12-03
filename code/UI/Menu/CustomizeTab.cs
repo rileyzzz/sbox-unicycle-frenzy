55f3df0af69c7a18f9bba22b6d8702b7897b4c90
Crayz <jacob.ee92@gmail.com>
2021-12-03 03:49:33 -0600

    Cleanup

--- code/UI/Menu/CustomizeTab.cs
+++ code/UI/Menu/CustomizeTab.cs
@@ -9,38 +9,20 @@ internal class CustomizeTab : Panel
 
 	public Panel RenderPanel { get; set; }
 
-	readonly ScenePanel scene;
-
-	Angles CamAngles = new( 25.0f, 0.0f, 0.0f );
-	float CamDistance = 50;
-	Vector3 CamPos => Vector3.Up * 22 + CamAngles.Direction * -CamDistance;
+	private ScenePanel renderScene;
+	private Angles renderSceneAngles = new( 25.0f, 0.0f, 0.0f );
+	private float renderSceneDistance = 50;
+	private Vector3 renderScenePos => Vector3.Up * 22 + renderSceneAngles.Direction * -renderSceneDistance;
 
 	public CustomizeTab()
 	{
-		Style.FlexWrap = Wrap.Wrap;
-		Style.JustifyContent = Justify.Center;
-		Style.AlignItems = Align.Center;
-		Style.AlignContent = Align.Center;
-
-		using ( SceneWorld.SetCurrent( new SceneWorld() ) )
-		{
-			SceneObject.CreateModel( "models/unicycle_dev.vmdl", Transform.Zero );
-
-			Light.Point( Vector3.Up * 150.0f, 200.0f, Color.White * 5.0f );
-			Light.Point( Vector3.Up * 10.0f + Vector3.Forward * 100.0f, 200, Color.White * 15.0f );
-			Light.Point( Vector3.Up * 10.0f + Vector3.Backward * 100.0f, 200, Color.White * 15f );
-			Light.Point( Vector3.Up * 10.0f + Vector3.Left * 100.0f, 200, Color.Orange * 15.0f );
-
-			scene = RenderPanel.Add.ScenePanel( SceneWorld.Current, CamPos, Rotation.From( CamAngles ), 60 );
-			scene.Style.Width = 512;
-			scene.Style.Height = 512;
-		}
+		BuildRenderScene();
 	}
 
 	public override void OnMouseWheel( float value )
 	{
-		CamDistance += value * 3;
-		CamDistance = CamDistance.Clamp( 10, 200 );
+		renderSceneDistance += value * 3;
+		renderSceneDistance = renderSceneDistance.Clamp( 10, 200 );
 
 		base.OnMouseWheel( value );
 	}
@@ -61,13 +43,30 @@ internal class CustomizeTab : Panel
 
 		if ( HasMouseCapture )
 		{
-			CamAngles.pitch += Mouse.Delta.y;
-			CamAngles.yaw -= Mouse.Delta.x;
-			CamAngles.pitch = CamAngles.pitch.Clamp( 0, 90 );
+			renderSceneAngles.pitch += Mouse.Delta.y;
+			renderSceneAngles.yaw -= Mouse.Delta.x;
+			renderSceneAngles.pitch = renderSceneAngles.pitch.Clamp( 0, 90 );
 		}
 
-		scene.CameraPosition = scene.CameraPosition.LerpTo( CamPos, 10f * Time.Delta );
-		scene.CameraRotation = Rotation.Lerp( scene.CameraRotation, Rotation.From( CamAngles ), 10f * Time.Delta );
+		renderScene.CameraPosition = renderScene.CameraPosition.LerpTo( renderScenePos, 10f * Time.Delta );
+		renderScene.CameraRotation = Rotation.Lerp( renderScene.CameraRotation, Rotation.From( renderSceneAngles ), 15f * Time.Delta );
+	}
+
+	private void BuildRenderScene()
+	{
+		using ( SceneWorld.SetCurrent( new SceneWorld() ) )
+		{
+			SceneObject.CreateModel( "models/unicycle_dev.vmdl", Transform.Zero );
+
+			Light.Point( Vector3.Up * 150.0f, 200.0f, Color.White * 5.0f );
+			Light.Point( Vector3.Up * 10.0f + Vector3.Forward * 100.0f, 200, Color.White * 15.0f );
+			Light.Point( Vector3.Up * 10.0f + Vector3.Backward * 100.0f, 200, Color.White * 15f );
+			Light.Point( Vector3.Up * 10.0f + Vector3.Left * 100.0f, 200, Color.Orange * 15.0f );
+
+			renderScene = RenderPanel.Add.ScenePanel( SceneWorld.Current, renderScenePos, Rotation.From( renderSceneAngles ), 60 );
+			renderScene.Style.Width = 512;
+			renderScene.Style.Height = 512;
+		}
 	}
 
 }
