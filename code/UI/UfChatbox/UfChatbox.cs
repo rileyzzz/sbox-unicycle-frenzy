de3e08a055f0a303bbeef1c8bf303ef4e3bd6cc0
Crayz <jacob.ee92@gmail.com>
2021-12-06 17:43:57 -0600

    Custom chatbox

---
+++ code/UI/UfChatbox/UfChatbox.cs
@@ -0,0 +1,103 @@
+﻿
+using Sandbox;
+using Sandbox.UI;
+
+[UseTemplate]
+public partial class UfChatbox : Panel
+{
+
+	public static UfChatbox Current;
+
+	public Panel EntryCanvas { get; set; }
+	public TextEntry Input { get; set; }
+
+	public bool IsOpen
+	{
+		get => HasClass( "open" );
+		set
+		{
+			SetClass( "open", value );
+			if ( value )
+			{
+				Input.Focus();
+				Input.Text = string.Empty;
+				Input.Label.SetCaretPosition( 0 );
+			}
+		}
+	}
+
+	public UfChatbox()
+	{
+		Current = this;
+
+		Sandbox.Hooks.Chat.OnOpenChat += () =>
+		{
+			IsOpen = !IsOpen;
+		};
+
+		EntryCanvas.PreferScrollToBottom = true;
+		EntryCanvas.TryScrollToBottom();
+
+		Input.AddEventListener( "onsubmit", Submit );
+		Input.AddEventListener( "onblur", () => IsOpen = false );
+	}
+
+	public override void OnHotloaded()
+	{
+		base.OnHotloaded();
+
+		ScrollToBottom();
+	}
+
+	protected override void PostTemplateApplied()
+	{
+		base.PostTemplateApplied();
+
+		ScrollToBottom();
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( !IsOpen ) return;
+
+		Input.Placeholder = string.IsNullOrEmpty( Input.Text ) ? "say something nice" : string.Empty;
+	}
+
+	public void AddEntry( string name, string message )
+	{
+		var entry = new UfChatboxEntry( name, message );
+		EntryCanvas.AddChild( entry );
+	}
+
+	private void Submit()
+	{
+		if ( string.IsNullOrWhiteSpace( Input.Text ) ) return;
+
+		SendChat( Input.Text );
+	}
+
+	private async void ScrollToBottom()
+	{
+		await Task.Delay( 1 );
+
+		EntryCanvas.TryScrollToBottom();
+	}
+
+	[ServerCmd]
+	public static void SendChat( string message )
+	{
+		if ( !ConsoleSystem.Caller.IsValid() ) return;
+
+		AddChat( To.Everyone, ConsoleSystem.Caller.Name, message );
+	}
+
+	[ClientCmd( "uf_chat_add", CanBeCalledFromServer = true )]
+	public static void AddChat( string name, string message )
+	{
+		Current?.AddEntry( name, message );
+	}
+
+}
+
---
+++ code/UI/UfChatbox/UfChatboxEntry.cs
@@ -0,0 +1,30 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+[UseTemplate]
+internal class UfChatboxEntry : Panel
+{
+
+	public Label Name { get; set; }
+	public Label Message { get; set; }
+
+	private TimeSince timeSinceCreation;
+
+	public UfChatboxEntry( string name, string message )
+	{
+		timeSinceCreation = 0;
+		Name.Text = name;
+		Message.Text = message;
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( timeSinceCreation < 8 ) return;
+
+		AddClass( "faded" );
+	}
+
+}
+
