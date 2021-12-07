e50476d70923a4b0cc67cef5ee9f22675eb7cf83
Crayz <jacob.ee92@gmail.com>
2021-12-07 12:03:15 -0600

    ControlsTab with input diagram

---
+++ code/UI/Menu/ControlsTab.cs
@@ -0,0 +1,26 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+[UseTemplate]
+[NavigatorTarget("menu/controls")]
+internal class ControlsTab : Panel
+{
+
+	public string BrakeBind => Input.GetKeyWithBinding( "+iv_duck" ) ?? "UNSET";
+	public string LeftPedalBind => Input.GetKeyWithBinding( "+iv_attack" ) ?? "UNSET";
+	public string RightPedalBind => Input.GetKeyWithBinding( "+iv_attack2" ) ?? "UNSET";
+	public string JumpBind => Input.GetKeyWithBinding( "+iv_jump" ) ?? "UNSET";
+	public string LeanBind => GetLeanBindString();
+
+	private string GetLeanBindString()
+	{
+		var fwd = Input.GetKeyWithBinding( "+iv_forward" ) ?? "UNSET";
+		var left = Input.GetKeyWithBinding( "+iv_left" ) ?? "UNSET";
+		var back = Input.GetKeyWithBinding( "+iv_back" ) ?? "UNSET";
+		var right = Input.GetKeyWithBinding( "+iv_right" ) ?? "UNSET";
+
+		return $"{fwd} {left} {back} {right}".ToUpper();
+	}
+
+}
+
