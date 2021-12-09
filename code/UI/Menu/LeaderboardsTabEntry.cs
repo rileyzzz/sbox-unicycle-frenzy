734acbd53cc246475a9af458b0a2a0a07f98d57d
Crayz <jacob.ee92@gmail.com>
2021-12-09 00:42:42 -0600

    Leaderboard ui mockup

---
+++ code/UI/Menu/LeaderboardsTabEntry.cs
@@ -0,0 +1,19 @@
+﻿using Sandbox.UI;
+
+[UseTemplate]
+internal class LeaderboardsTabEntry : Panel
+{
+
+	public string PlayerName { get; set; }
+	public string Rank { get; set; }
+	public string Time { get; set; }
+
+	public LeaderboardsTabEntry( int rank, string name, float time )
+	{
+		PlayerName = name;
+		Rank = rank.ToString();
+		Time = System.TimeSpan.FromSeconds( time ).ToString( @"m\:ss" );
+	}
+
+}
+
