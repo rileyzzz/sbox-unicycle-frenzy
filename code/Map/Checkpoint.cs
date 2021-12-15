54eb7c2e94ff6213e0d67a79fcdce9509976f6c4
bakscratch <lt_8@hotmail.co.uk>
2021-12-15 13:34:50 +0000

    Checkpoints now show flag if hit the checkpoint

--- code/Map/Checkpoint.cs
+++ code/Map/Checkpoint.cs
@@ -2,26 +2,28 @@
 using Sandbox;
 using Sandbox.Internal;
 
-[Library( "uf_checkpoint", Description = "Defines a checkpoint where the player will respawn after falling" )]
-[EditorModel( "models/editor/playerstart.vmdl", staticGreen: 100, FixedBounds = true )]
-[EntityTool( "Player Checkpoint", "Unicycle Frenzy", "Defines a checkpoint where the player will respawn after falling" )]
-[BoundsHelper( "mins", "maxs", false, true )]
+[Library("uf_checkpoint", Description = "Defines a checkpoint where the player will respawn after falling")]
+[EditorModel("models/editor/playerstart.vmdl", staticGreen: 100, FixedBounds = true)]
+[EntityTool("Player Checkpoint", "Unicycle Frenzy", "Defines a checkpoint where the player will respawn after falling")]
+[BoundsHelper("mins", "maxs", false, true)]
 internal partial class Checkpoint : ModelEntity
 {
 
-	[Property( "mins", Title = "Checkpoint mins" )]
-	[DefaultValue( "-75 -75 0" )]
+	[Property("mins", Title = "Checkpoint mins")]
+	[DefaultValue("-75 -75 0")]
 	[Net]
-	public Vector3 Mins { get; set; } = new Vector3( -75, -75, 0 );
-	[Property( "maxs", Title = "Checkpoint maxs" )]
-	[DefaultValue( "75 75 100" )]
+	public Vector3 Mins { get; set; } = new Vector3(-75, -75, 0);
+	[Property("maxs", Title = "Checkpoint maxs")]
+	[DefaultValue("75 75 100")]
 	[Net]
-	public Vector3 Maxs { get; set; } = new Vector3( 75, 75, 100 );
+	public Vector3 Maxs { get; set; } = new Vector3(75, 75, 100);
 
 	[Net, Property]
 	public bool IsStart { get; set; }
 	[Net, Property]
 	public bool IsEnd { get; set; }
+
+	public ModelEntity FlagModel;
 
 	public override void Spawn()
 	{
@@ -39,23 +41,24 @@ internal partial class Checkpoint : ModelEntity
 	public override void ClientSpawn()
 	{
 		base.ClientSpawn();
-		
-		var modelEnt = new ModelEntity("models/flag/flag_pole.vmdl");
+
+		//var modelEnt = new ModelEntity("models/flag/flag_pole.vmdl");
+		FlagModel = new ModelEntity("models/flag/flag_pole.vmdl");
 		var fwdLeft = Rotation.Forward + Rotation.Left;
 		var bounds = new BBox( Position + Mins, Position + Maxs );
-		modelEnt.Position = bounds.ClosestPoint( Position + fwdLeft * 500 ) - fwdLeft.Normal * 5;
-		modelEnt.Rotation = Rotation.LookAt( Rotation.Right );
+		FlagModel.Position = bounds.ClosestPoint( Position + fwdLeft * 500 ) - fwdLeft.Normal * 5;
+		FlagModel.Rotation = Rotation.LookAt( Rotation.Right );
 
 		if (this.IsStart)
 		{
-			modelEnt.SetMaterialGroup("Green");
-			modelEnt.SetModel("models/flag/flag.vmdl");
+			FlagModel.SetModel("models/flag/flag.vmdl");
+			FlagModel.SetMaterialGroup("Green");
 		}
 
 		if (this.IsEnd)
 		{
-			modelEnt.SetMaterialGroup("Checker");
-			modelEnt.SetModel("models/flag/flag.vmdl");
+			FlagModel.SetModel("models/flag/flag.vmdl");
+			FlagModel.SetMaterialGroup("Checker");
 		}
 
 	}
@@ -64,7 +67,6 @@ internal partial class Checkpoint : ModelEntity
 	{
 		base.StartTouch( other );
 
-		
 
 		if ( other is not UnicyclePlayer pl ) return;
 
@@ -99,8 +101,11 @@ internal partial class Checkpoint : ModelEntity
 	private void OnFrame()
 	{
 		var color = Color.White;
-		if ( Local.Pawn is UnicyclePlayer pl && pl.Checkpoints.Contains( this ) )
+		if (Local.Pawn is UnicyclePlayer pl && pl.Checkpoints.Contains(this))
+		{
+			FlagModel.SetModel("models/flag/flag.vmdl");
 			color = Color.Red;
+		}
 
 		DebugOverlay.Box( Position + Mins, Position + Maxs, color );
 
