8464697a2606f48df639881cef360d6a2e2de265
Crayz <jacob.ee92@gmail.com>
2022-01-18 12:02:06 -0700

    Update Controls page to show binds with Input.GetButtonOrigin

--- code/UI/Menu/ControlsTab.cs
+++ code/UI/Menu/ControlsTab.cs
@@ -6,21 +6,21 @@ using Sandbox.UI;
 internal class ControlsTab : Panel
 {
 
-	public string BrakeBind => Input.GetKeyWithBinding( "+iv_duck" ) ?? "UNSET";
-	public string LeftPedalBind => Input.GetKeyWithBinding( "+iv_attack" ) ?? "UNSET";
-	public string RightPedalBind => Input.GetKeyWithBinding( "+iv_attack2" ) ?? "UNSET";
-	public string JumpBind => Input.GetKeyWithBinding( "+iv_jump" ) ?? "UNSET";
+	public string BrakeBind => Input.GetButtonOrigin( InputButton.Duck ) ?? "UNSET";
+	public string LeftPedalBind => Input.GetButtonOrigin( InputButton.Attack1 ) ?? "UNSET";
+	public string RightPedalBind => Input.GetButtonOrigin( InputButton.Attack2 ) ?? "UNSET";
+	public string JumpBind => Input.GetButtonOrigin( InputButton.Jump ) ?? "UNSET";
 	public string LeanBind => GetLeanBindString();
-	public string RestartBind => Input.GetKeyWithBinding( "+iv_drop" ) ?? "UNSET";
-	public string GoBackBind => Input.GetKeyWithBinding( "+iv_reload" ) ?? "UNSET";
-	public string SprayBind => Input.GetKeyWithBinding( "+iv_flashlight" ) ?? "UNSET";
+	public string RestartBind => Input.GetButtonOrigin( InputButton.Drop ) ?? "UNSET";
+	public string GoBackBind => Input.GetButtonOrigin( InputButton.Reload ) ?? "UNSET";
+	public string SprayBind => Input.GetButtonOrigin( InputButton.Flashlight ) ?? "UNSET";
 
 	private string GetLeanBindString()
 	{
-		var fwd = Input.GetKeyWithBinding( "+iv_forward" ) ?? "UNSET";
-		var left = Input.GetKeyWithBinding( "+iv_left" ) ?? "UNSET";
-		var back = Input.GetKeyWithBinding( "+iv_back" ) ?? "UNSET";
-		var right = Input.GetKeyWithBinding( "+iv_right" ) ?? "UNSET";
+		var fwd = Input.GetButtonOrigin( InputButton.Forward ) ?? "UNSET";
+		var left = Input.GetButtonOrigin( InputButton.Left ) ?? "UNSET";
+		var back = Input.GetButtonOrigin( InputButton.Back ) ?? "UNSET";
+		var right = Input.GetButtonOrigin( InputButton.Right ) ?? "UNSET";
 
 		return $"{fwd} {left} {back} {right}".ToUpper();
 	}
