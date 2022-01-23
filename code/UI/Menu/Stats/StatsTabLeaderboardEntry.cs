788067b40bcdcb9bbf5dd9bb7c77f9a70f2dc257
Crayz <jacob.ee92@gmail.com>
2022-01-22 21:30:46 -0700

    Populate global leaderboard tab with top times

---
+++ code/UI/Menu/Stats/StatsTabLeaderboardEntry.cs
@@ -0,0 +1,18 @@
+﻿using Sandbox.UI;
+
+[UseTemplate]
+internal class StatsTabLeaderboardEntry : Panel
+{
+
+	public int Rank { get; set; }
+	public string Name { get; set; }
+	public string Time { get; set; }
+
+	public StatsTabLeaderboardEntry( int rank, string name, float score )
+	{
+		Rank = rank;
+		Name = name;
+		Time = CourseTimer.FormattedTimeMsf( score );
+	}
+
+}
--- code/UI/Menu/Stats/StatsTabLeaderboardSingle.cs
+++ code/UI/Menu/Stats/StatsTabLeaderboardSingle.cs
@@ -1,11 +1,77 @@
 ﻿using Sandbox.UI;
+using System;
+using System.Linq;
 
 [UseTemplate]
 [NavigatorTarget( "menu/stats/leaderboard/{scope}" )]
 internal class StatsTabLeaderboardSingle : NavigatorPanel
 {
 
+	private enum LeaderboardScope
+	{
+		None,
+		Session,
+		Global,
+		Friends
+	}
 
+	private LeaderboardScope scope = LeaderboardScope.None;
+
+	public Panel Canvas { get; set; }
+
+	public override void SetProperty( string name, string value )
+	{
+		base.SetProperty( name, value );
+
+		if( string.Equals(name, "scope" ) )
+		{
+			scope = Enum.Parse<LeaderboardScope>( value, true );
+
+			RebuildLeaderboard();
+		}
+	}
+
+	protected override void PostTemplateApplied()
+	{
+		base.PostTemplateApplied();
+
+		RebuildLeaderboard();
+	}
+
+	public override void OnHotloaded()
+	{
+		base.OnHotloaded();
+
+		RebuildLeaderboard();
+	}
+
+	public void Rebuild()
+	{
+		RebuildLeaderboard();
+	}
+
+	private async void RebuildLeaderboard()
+	{
+		if ( scope == LeaderboardScope.None ) return;
+
+		Canvas.DeleteChildren( true );
+
+		switch ( scope )
+		{
+			case LeaderboardScope.Global:
+				var q = await GameServices.Leaderboard.Query( game: Global.GameName, bucket: Global.MapName );
+				var sorted = q.Entries.OrderBy( x => x.Rating );
+				var rank = 1;
+
+				foreach ( var entry in sorted )
+				{
+					var el = new StatsTabLeaderboardEntry( rank, entry.DisplayName, entry.Rating );
+					el.Parent = Canvas;
+					rank++;
+				}
+				break;
+		}
+	}
 
 }
 
