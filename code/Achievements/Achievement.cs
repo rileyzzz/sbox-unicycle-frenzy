2a50c2400da2b9039c522b3a3d023d4f7c744070
Crayz <jacob.ee92@gmail.com>
2022-01-25 18:19:07 -0700

    Model out achievements

---
+++ code/Achievements/Achievement.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+internal class Achievement
+{
+
+	public long AchievementId { get; set; }
+	public string GameName { get; set; }
+	// we'd use these in api instead of game/map name
+	//public long GameAssetId { get; set; } 
+	//public long MapAssetId { get; set; }
+	public string ShortName { get; set; }
+	public string DisplayName { get; set; }
+	public string Description { get; set; }
+	public string ImageThumb { get; set; }
+
+	public void Set( long playerid )
+	{
+		// later: push to api
+		var completion = AchievementCompletion.Query( playerid, Global.GameName )
+			.FirstOrDefault( x => x.AchievementId == AchievementId );
+
+		// no duplicate achievements
+		if ( completion != null ) return;
+
+		AchievementCompletion.Insert( playerid, AchievementId );
+	}
+
+	public static IEnumerable<Achievement> Query( string game, string shortname = null )
+	{
+		// later: fetch from api
+		var result = All.Where( x => x.GameName == game );
+
+		if ( shortname == null ) return result;
+
+		return result.Where( x => x.ShortName == shortname );
+	}
+
+	public static List<Achievement> All
+	{
+		get
+		{
+			// later: fetch from api
+			var result = new List<Achievement>();
+
+			result.Add( new Achievement()
+			{
+				AchievementId = 1,
+				Description = "Complete any map in Unicycle Frenzy",
+				DisplayName = "Unicyclist",
+				ShortName = "uf_unicyclist",
+				GameName = Global.GameName,
+				ImageThumb = "https://files.facepunch.com/crayz/1b2511b1/msedge_2022-01-25_17-06-28.png"
+			} );
+
+			return result;
+		}
+	}
+
+}
---
+++ code/Achievements/AchievementCompletion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Linq;
+
+internal class AchievementCompletion
+{
+
+	public long AchievementId { get; set; } // key
+	public long SteamId { get; set; } // key
+	public DateTimeOffset Time { get; set; }
+
+	public static List<AchievementCompletion> Query( long playerid, string game )
+	{
+		// later: fetch from api
+		var result = new List<AchievementCompletion>();
+
+		foreach( var completion in All.Where( x => x.SteamId == playerid ) )
+		{
+			var ach = Achievement.All.FirstOrDefault( x => x.AchievementId == completion.AchievementId );
+			if ( ach == null ) continue;
+			if ( ach.GameName != game ) continue;
+			result.Add( completion );
+		}
+
+		return result;
+	}
+
+	public static void Insert( long playerid, long achievementid )
+	{
+		if ( playerid != Sandbox.Local.PlayerId ) return;
+
+		var final = new List<AchievementCompletion>( All )
+		{
+			new AchievementCompletion()
+			{
+				AchievementId = achievementid,
+				SteamId = playerid,
+				Time = DateTime.Now
+			}
+		};
+
+		Cookie.Set( "uf.achievement_completions", JsonSerializer.Serialize( final ) );
+	}
+
+	public static List<AchievementCompletion> All => JsonSerializer.Deserialize<List<AchievementCompletion>>( Cookie.Get( "uf.achievement_completions", "{}" ) );
+
+}
