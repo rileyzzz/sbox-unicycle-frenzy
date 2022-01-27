0fc02171acecdf63b480842c548d78db1fcf1063
Crayz <jacob.ee92@gmail.com>
2022-01-27 15:13:40 -0700

    Add a Spectate button to leaderboards if the player is in game

--- code/UI/Menu/Stats/StatsTabLeaderboardEntry.cs
+++ code/UI/Menu/Stats/StatsTabLeaderboardEntry.cs
@@ -1,5 +1,6 @@
 ﻿using Sandbox;
 using Sandbox.UI;
+using System.Linq;
 
 [UseTemplate]
 internal class StatsTabLeaderboardEntry : Panel
@@ -9,14 +10,27 @@ internal class StatsTabLeaderboardEntry : Panel
 	public string Name { get; set; }
 	public string Time { get; set; }
 
+	private long playerid;
+
 	public StatsTabLeaderboardEntry( int rank, string name, float score, long playerId = 0 )
 	{
 		Rank = rank;
 		Name = name;
 		Time = score == 0f ? "INCOMPLETE" : CourseTimer.FormattedTimeMsf( score );
+		playerid = playerId;
 
 		SetClass( "me", Local.PlayerId == playerId );
 		SetClass( "friend", new Friend( playerId ).IsFriend );
+		SetClass( "can-spectate", Client.All.Any( x => x.PlayerId == playerId ) );
+	}
+
+	public void SpectateThisPlayer()
+	{
+		var cl = Client.All.FirstOrDefault( x => x.PlayerId == playerid );
+		if ( cl == null ) return;
+		if ( !cl.Pawn.IsValid() ) return;
+
+		UnicyclePlayer.SetSpectateTargetOnServer( cl.Pawn.NetworkIdent );
 	}
 
 }
