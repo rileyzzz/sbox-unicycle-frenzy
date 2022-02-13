192ee721715b49fb3fe49f71931ae7d505be905e
Crayz <jacob.ee92@gmail.com>
2022-02-13 12:23:26 -0700

    Trigger for enabling specific tutorial features

---
+++ code/Map/TutorialTrigger.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+using Hammer;
+
+[Library( "uf_trigger_tutorial", Description = "Enable tutorial features in this trigger" )]
+//[Hammer.AutoApplyMaterial( "materials/editor/uf_trigger_fall.vmat" )]
+[EntityTool( "Trigger Tutorial", "Unicycle Frenzy", "Enable tutorial features in this trigger." )]
+internal partial class TutorialTrigger : BaseTrigger
+{
+
+	[Property]
+	public bool PerfectPedalGlow { get; set; }
+
+	public override void Spawn()
+	{
+		base.Spawn();
+
+		Transmit = TransmitType.Always;
+	}
+
+	public override void StartTouch( Entity other )
+	{
+		base.StartTouch( other );
+
+
+	}
+
+	public override void EndTouch( Entity other )
+	{
+		base.EndTouch( other );
+
+
+	}
+
+}
