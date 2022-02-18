24aae2da5e7d92179a7b11f02545be4ec5c5623d
Crayz <jacob.ee92@gmail.com>
2022-02-17 17:22:45 -0700

    Show toast when granted xp

---
+++ code/UI/Toast/Toast.cs
@@ -0,0 +1,34 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+[UseTemplate]
+internal class Toast : Panel
+{
+
+	private TimeSince timeSinceCreated = 0;
+
+	public Button Message { get; set; }
+
+	public Toast( string message, Toaster.ToastTypes type )
+	{
+		AddClass( type.ToString() );
+		Message.Text = message;
+		Message.Icon = type switch
+		{
+			Toaster.ToastTypes.Celebrate => "celebration",
+			Toaster.ToastTypes.Affirm => "check",
+			Toaster.ToastTypes.Award => "star",
+			_ => "info"
+		};
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( timeSinceCreated < 8 ) return;
+
+		Delete();
+	}
+
+}
---
+++ code/UI/Toast/Toaster.cs
@@ -0,0 +1,35 @@
+﻿using Sandbox;
+using Sandbox.UI;
+
+[UseTemplate]
+internal class Toaster : Panel
+{
+
+	public enum ToastTypes
+	{
+		Simple,
+		Celebrate,
+		Affirm,
+		Award
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( Input.Pressed( InputButton.Slot1 ) )
+		{
+			Toast( "Test celebration", ToastTypes.Celebrate );
+		}
+
+		if ( Input.Pressed( InputButton.Slot2 ) )
+		{
+			Toast( "Test award", ToastTypes.Award );
+		}
+	}
+
+	private static Toaster instance;
+	public Toaster() => instance = this;
+	public static void Toast( string message, ToastTypes type ) => new Toast( message, type ).Parent = instance;
+
+}
