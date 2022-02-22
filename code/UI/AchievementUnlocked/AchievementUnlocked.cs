85c41d01fd56c765a5f7644014934740f69c1546
Crayz <jacob.ee92@gmail.com>
2022-02-21 22:55:21 -0700

    Show popup when you unlock an achievement

---
+++ code/UI/AchievementUnlocked/AchievementUnlocked.cs
@@ -0,0 +1,47 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using System.Linq;
+
+[UseTemplate]
+internal class AchievementUnlocked : Panel
+{
+
+	private TimeSince timeSinceDisplayed;
+
+	public Panel Icon { get; set; }
+	public Label DisplayName { get; set; }
+	public Label Experience { get; set; }
+
+	public void Display( Achievement achievement )
+	{
+		Icon.Style.SetBackgroundImage( achievement.ImageThumb );
+		DisplayName.Text = achievement.DisplayName;
+		Experience.Text = string.Empty;
+
+		var bpAch = TrailPass.Current.Achievements.FirstOrDefault( x => x.AchievementShortName == achievement.ShortName );
+		if( bpAch != null )
+		{
+			Experience.Text = $"+{bpAch.ExperienceGranted}xp";
+		}
+
+		SetClass( "open", true );
+		timeSinceDisplayed = 0;
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		SetClass( "open", timeSinceDisplayed < 6f );
+	}
+
+	[Event("achievement.set")]
+	public void OnAchievementSet( string shortname )
+	{
+		var ach = Achievement.All.FirstOrDefault( x => x.ShortName == shortname );
+		if ( ach == null ) return;
+
+		Display( ach );
+	}
+
+}
