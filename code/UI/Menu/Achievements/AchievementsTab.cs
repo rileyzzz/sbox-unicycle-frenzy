aa63e52edffae6de972ad76289f44af6e50bcbba
Lewis Ainslie <hello@lewwis.com>
2022-03-08 11:44:48 +0000

    Achievements Tab

---
+++ code/UI/Menu/Achievements/AchievementsTab.cs
@@ -0,0 +1,13 @@
+using Sandbox.UI;
+
+[UseTemplate]
+[NavigatorTarget("menu/achievements")]
+internal class AchievementsTab : NavigatorPanel
+{
+
+    public AchievementsTab()
+    {
+        //Navigate( "menu/stats/details" );
+    }
+
+}
