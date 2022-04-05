5a09ad0f721b60e466719bc119cd5be47064e145
Jake Evans <jacob.ee92@gmail.com>
2022-04-05 16:01:24 -0600

    End-game render scenes for top 3 players

--- code/UI/GameEnd/PodiumPanel.cs
+++ code/UI/GameEnd/PodiumPanel.cs
@@ -1,12 +1,14 @@
 ﻿
 using Sandbox;
 using Sandbox.UI;
+using System;
 using System.Linq;
 
 [UseTemplate]
 internal class PodiumPanel : Panel
 {
 
+	public Panel RenderCanvas { get; set; }
 	public int Rank { get; }
 	public string Name { get; private set; }
 	public string Time { get; private set; }
@@ -16,26 +18,44 @@ internal class PodiumPanel : Panel
 		Rank = rank;
 
 		AddClass( rank.ToString() );
-		UpdateData();
 	}
 
+	private int activehash;
 	public override void Tick()
 	{
 		base.Tick();
 
-		UpdateData();
-	}
-
-	private void UpdateData()
-	{
 		var players = Player.All.Where( x => x is UnicyclePlayer && x.IsValid() && x.Client.IsValid() ).ToList();
 		var orderedPlayers = players.OrderBy( x => (x as UnicyclePlayer).BestTime );
 
 		var player = orderedPlayers.Skip( Rank - 1 ).FirstOrDefault() as UnicyclePlayer;
 		if ( player == null ) return;
 
-		Name = player.Client.Name;
-		Time = CourseTimer.FormattedTimeMsf( player.BestTime );
+		var newhash = HashCode.Combine( player.BestTime, player.SessionRank, player.NetworkIdent );
+		if ( newhash == activehash ) return;
+
+		activehash = newhash;
+
+		UpdateData( player );
+	}
+
+	private void UpdateData( UnicyclePlayer pl )
+	{
+		RenderCanvas.DeleteChildren( true );
+
+		var renderscene = new PodiumRenderScene( pl );
+		var scale = Rank switch
+		{
+			1 => 1.25f,
+			2 => 1f,
+			3 => .75f,
+			_ => 1f
+		};
+		renderscene.Style.Set( $"transform: scale({scale});" );
+		RenderCanvas.AddChild( renderscene );
+
+		Name = pl.Client.Name;
+		Time = CourseTimer.FormattedTimeMsf( pl.BestTime );
 	}
 
 }
--- code/UI/GameEnd/PodiumRenderScene.cs
+++ code/UI/GameEnd/PodiumRenderScene.cs
@@ -1,10 +1,52 @@
 ﻿
+
 using Sandbox;
 using Sandbox.UI;
+using Sandbox.UI.Construct;
 
 internal class PodiumRenderScene : Panel
 {
 
+	private UnicyclePlayer player;
+	private ScenePanel ScenePanel;
+	private SceneWorld SceneWorld;
 
+	public PodiumRenderScene( UnicyclePlayer pl )
+	{
+		player = pl;
+
+		Build();
+	}
+
+	private void Build()
+	{
+		Assert.True( player.IsValid() );
+
+		SceneWorld?.Delete();
+		ScenePanel?.Delete();
+
+		SceneWorld = new SceneWorld();
+		ScenePanel = Add.ScenePanel( SceneWorld, Vector3.Backward * 75 + Vector3.Up * 52, Rotation.Identity, 75 );
+		ScenePanel.CameraRotation = Rotation.FromPitch( 10 );
+
+		ScenePanel.Style.Width = Length.Percent( 100 );
+		ScenePanel.Style.Height = Length.Percent( 100 );
+
+		new SceneModel( SceneWorld, "models/citizen/citizen.vmdl", Transform.Zero.WithRotation( Rotation.FromYaw( 180 ) ) );
+		new SceneLight( SceneWorld, Vector3.Backward * 50 + Vector3.Up * 50, 200f, Color.White * 10 );
+	}
+
+	public override void Tick()
+	{
+		base.Tick();
+
+		if ( !SceneWorld.IsValid() ) return;
+
+		foreach(var obj in SceneWorld.SceneObjects )
+		{
+			if ( obj is not SceneModel m ) continue;
+			m.Update( RealTime.Delta );
+		}
+	}
 
 }
