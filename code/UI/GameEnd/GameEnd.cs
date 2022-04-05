3c68ff89375a329496cf1d192785a7a6d7e5a5a5
Jake Evans <jacob.ee92@gmail.com>
2022-04-05 15:17:57 -0600

    Cleanup, fix incorrect hash check

--- code/UI/GameEnd/GameEnd.cs
+++ code/UI/GameEnd/GameEnd.cs
@@ -1,7 +1,6 @@
 ﻿using Sandbox;
 using Sandbox.UI;
 using System;
-using System.Linq;
 
 [UseTemplate]
 internal class GameEnd : Panel
@@ -10,40 +9,6 @@ internal class GameEnd : Panel
 	public Panel MapCanvas { get; set; }
 	public string TimeLeft => CourseTimer.FormattedTimeMs( UnicycleFrenzy.Game.GameTimer );
 
-	public string FirstName { get; set; }
-	public string FirstTime { get; set; }
-	public string SecondName { get; set; }
-	public string SecondTime { get; set; }
-	public string ThirdName { get; set; }
-	public string ThirdTime { get; set; }
-
-	protected override void PostTemplateApplied()
-	{
-		base.PostTemplateApplied();
-		
-		RefreshMaps();
-	}
-
-	public override void OnHotloaded()
-	{
-		base.OnHotloaded();
-
-		RefreshMaps();
-	}
-
-	private void RefreshMaps()
-	{
-		MapCanvas.DeleteChildren();
-
-		foreach ( var map in UnicycleFrenzy.Game.MapOptions )
-		{
-			var btn = new MapVoteButton( map );
-			MapCanvas.AddChild( btn );
-		}
-	}
-
-	private int maphash = 0;
-		
 	[Event.Frame]
 	private void OnFrame()
 	{
@@ -54,49 +19,37 @@ internal class GameEnd : Panel
 		if ( !open ) return;
 
 		EnsureMaps();
-
-		var players = Player.All.Where( x => x is UnicyclePlayer && x.IsValid() && x.Client.IsValid() ).ToList();
-		var orderedPlayers = players.OrderBy( x => (x as UnicyclePlayer).BestTime );
-
-		int rank = 1;
-
-		foreach ( var player in orderedPlayers )
-		{
-			var pl = player as UnicyclePlayer;
-			switch ( rank )
-			{
-				case 1:
-					FirstName = $"#1 " + pl.Client.Name;
-					FirstTime = pl.CourseIncomplete ? "INCOMPLETE" : CourseTimer.FormattedTimeMsf( pl.BestTime );
-					break;
-				case 2:
-					SecondName = $"#2 " + pl.Client.Name;
-					SecondTime = pl.CourseIncomplete ? "INCOMPLETE" : CourseTimer.FormattedTimeMsf( pl.BestTime );
-					break;
-				case 3:
-					ThirdName = $"#3 " + pl.Client.Name;
-					ThirdTime = pl.CourseIncomplete ? "INCOMPLETE" : CourseTimer.FormattedTimeMsf( pl.BestTime );
-					break;
-				default: 
-					return;
-			}
-			rank++;
-		}
 	}
 
+	private int maphash = 0;
 	private void EnsureMaps()
 	{
 		var newhash = 0;
-		foreach ( var map in UnicycleFrenzy.Game.MapOptions )
+		var options = UnicycleFrenzy.Game.MapOptions;
+
+		foreach ( var map in options )
 		{
-			newhash = HashCode.Combine( map );
+			newhash = HashCode.Combine( newhash, map );
 		}
 
-		if ( newhash != maphash )
+		if ( newhash == maphash ) return;
+
+		maphash = newhash;
+		RefreshMaps();
+	}
+
+	private void RefreshMaps()
+	{
+		MapCanvas.DeleteChildren( true );
+
+		foreach ( var map in UnicycleFrenzy.Game.MapOptions )
 		{
-			RefreshMaps();
-			maphash = newhash;
+			var btn = new MapVoteButton( map );
+			MapCanvas.AddChild( btn );
 		}
 	}
+
+	protected override void PostTemplateApplied() => RefreshMaps();
+	public override void OnHotloaded() => RefreshMaps();
 
 }
