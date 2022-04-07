5806a4d2fa41dc4df19c58dffaf6a8dea4ae1b99
Jake Evans <jacob.ee92@gmail.com>
2022-04-06 20:38:56 -0600

    Animator sets aim_eyes and aim_head

--- code/Player/UnicycleAnimator.cs
+++ code/Player/UnicycleAnimator.cs
@@ -15,7 +15,11 @@ internal class UnicycleAnimator : PawnAnimator
         var target = pl.Terry;
         var unicycle = pl.Unicycle;
 
-        var leftpos = unicycle.DisplayedLeftPedal?.GetAttachment( "foot" )?.Position ?? Vector3.Zero;
+		var aimPos = Pawn.EyePosition + Input.Rotation.Forward * 200 + Vector3.Up * 50;
+		pl.Terry.SetAnimLookAt( "aim_eyes", aimPos );
+		pl.Terry.SetAnimLookAt( "aim_head", aimPos );
+
+		var leftpos = unicycle.DisplayedLeftPedal?.GetAttachment( "foot" )?.Position ?? Vector3.Zero;
         var rightpos = unicycle.DisplayedRightPedal?.GetAttachment( "foot" )?.Position ?? Vector3.Zero;
         leftpos = target.Transform.PointToLocal( leftpos + Rotation.Up * 5 );
         rightpos = target.Transform.PointToLocal( rightpos + Rotation.Up * 5 );
